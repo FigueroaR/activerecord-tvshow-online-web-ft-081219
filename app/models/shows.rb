2ebb7b0a440f91255f94648fcb5d1cdc7fea4a5b
Raul Figueroa <raul.a.figueroa@hotmail.com>
2019-10-16 19:43:47 +0000

    Automatically backed up by Learn

--- app/models/shows.rb
+++ app/models/shows.rb
@@ -20,7 +20,7 @@ class Show < ActiveRecord::Base
   end 
   
   def Show::popular_shows
-    Show.order("rating > 5")
+    Show.where("rating > 5")
   end 
   
   def Show::shows_by_alphabetical_order
